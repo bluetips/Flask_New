81bae7e6a092dbf2a72692ce892843c0b7ed3115
bluetips <1730116525@qq.com>
2018-12-05 14:26:47 +0800

    完成新闻列表的后端实现，实现接口 接收格式 json 接收参数 分类id 当前页面 每页显示数量 校验参数 数据库查询 返回json

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 
 from info import constants
 from info.models import User, News
+from info.utils.response_code import RET
 from . import index_blu
 
 
@@ -29,7 +30,7 @@ def index():
         user = User.query.get(user_id)
     data = {
         'user_info': user.to_dict() if user else None,
-        'news_hot_list':news_hot_list
+        'news_hot_list': news_hot_list
     }
 
     return render_template('news/index.html', data=data)
@@ -38,3 +39,54 @@ def index():
 @index_blu.route('/favicon.ico')
 def favicon():
     return current_app.send_static_file('news/favicon.ico')
+
+
+@index_blu.route('/new_list')
+def new_list():
+    """
+    GET request.arg
+    1.参数：分类cid 页面page 每页多少content_num
+    2.参数校验
+    3.数据库查询
+    4.返回json
+    :return:
+    """
+    args_dict = request.args
+    cid = args_dict.get('cid', '1')
+    page = args_dict.get('page', '1')
+    per_page = args_dict.get('per_page', constants.HOME_PAGE_MAX_NEWS)
+
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg='参数格式错误')
+
+    # 如果分类id不为1，那么添加分类id的过滤
+    filters = []
+
+    if cid != '1':
+        filters.append(News.category_id == cid)
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        items = paginate.items
+        tottal_page = paginate.pages
+        current_page = paginate.page
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据库错误')
+
+    new_list = []
+
+    for new in items:
+        new_list.append(new.to_dict())
+    data = {
+        'tottal_page': tottal_page,
+        'current_page': current_page,
+        'new_list': new_list,
+        'cid': cid
+
+    }
+
+    return jsonify(errno=RET.OK, errmsg='成功', data=data)
